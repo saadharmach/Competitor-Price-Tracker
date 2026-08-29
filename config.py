1ff9c64c7cdc68f81842d4ee39a229dc00109b47
saadbavo <saadhmk13@gmail.com>
2026-08-29 01:17:06 +0100

    Fix the struct: Moved app.py to dashpoard folder Rename raw folder

---
+++ config.py
@@ -0,0 +1,13 @@
+
+stores = [
+    {
+        "store_name": "Store A",
+        "store_url": "https://store-a.com",
+        "url_format": "https://store-a.com/products/{product_id}",
+        "selectors": {
+            "product_name": ".product-title",
+            "price": ".product-price",
+            "availability": ".product-availability"
+        }
+    }
+]
